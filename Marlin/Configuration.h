fbb1124ec3b9ce4fe7112047388f12e51385053a
iliDaedalus <daedalus.i@gmail.com>
2024-11-27 12:27:52 +0100

    Increased the Z_MAX_POS to 240

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1750,7 +1750,7 @@
 #define X_MIN_POS -3
 #define Y_MIN_POS -5
 #define Z_MIN_POS 0
-#define Z_MAX_POS 230
+#define Z_MAX_POS 240
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 // #define Z_MAX_POS 200
